8f1e8762c2f52f1e885c1da0ea1f116bc38e554f
MrNiza <65722254+MrNiza@users.noreply.github.com>
2021-06-01 21:30:05 -0300

    Add files via upload

---
+++ mica-tp/CentroVacunacion/src/trabajoPractico/VacunaMenos18.java
@@ -0,0 +1,8 @@
+package trabajoPractico;
+
+public class VacunaMenos18 extends Vacuna{
+	
+	public VacunaMenos18 (String nombre, boolean prioridadMayores, Fecha fechaIngreso) {
+		super (nombre, -18, fechaIngreso, prioridadMayores);
+	}
+}
